c89c817eea3c4c481830761f0c712dc6b5d72c2c
Adam Amer <adamamer1801@gmail.com>
2022-07-06 22:11:03 +0300

    Added shortenings

--- src/discord/Client.ts
+++ src/discord/Client.ts
@@ -1,12 +1,14 @@
-import { Client, Interaction } from 'discord.js';
+import { Client, Interaction, Message } from 'discord.js';
 import { Intents } from 'discord.js';
 import log, { errorLog } from '../utils/logger';
 import handle from './CommandHandler'
+import msgHandle from './MessageHandler';
 
-const intents = [Intents.FLAGS.GUILDS, Intents.FLAGS.DIRECT_MESSAGES];
+const intents = [Intents.FLAGS.GUILDS, Intents.FLAGS.DIRECT_MESSAGES, Intents.FLAGS.GUILD_MESSAGES];
 const client = new Client({ intents: intents });
 
 client.on('interactionCreate', async (i: Interaction) => await handle(i))
+client.on('messageCreate', async (m: Message) => await msgHandle(m))
 
 function clientLogin(token: string): void {
     try { client.login(token) }
---
+++ src/discord/MessageHandler.ts
@@ -0,0 +1,14 @@
+import { CommandInteraction, Interaction, Message } from "discord.js";
+import config from "../../conf";
+import { shortenings } from "../../data/shortenings/shortenings";
+import Command, { CommandClassInstances } from "../classes/Command";
+
+async function msgHandle(m: Message) {
+    shortenings.forEach((shortening: string[]) => {
+        if (shortening[0].toUpperCase() == m.content.toUpperCase()) {
+            m.channel.send(shortening[1])
+        }
+    })
+}
+
+export default msgHandle;
